5e6c7f752c039586ca70094c1831e56346a3a697
100215 <100215@glr.nl>
2024-06-12 14:55:20 +0200

    hello

---
+++ classes/Album.php
@@ -0,0 +1,117 @@
+<?php
+declare(strict_types=1);
+class Album
+{
+    private int $id;
+    private string $naam;
+    private string $artiesten;
+    private string $release_datum;
+    private string $url;
+    private string $afbeelding;
+    private string $prijs;
+    public function __construct(int $id, string $naam, string $artiesten, string $release_datum, string $url, string $afbeelding, string $prijs)
+{
+    $this->id = $id;
+    $this->naam = $naam;
+    $this->artiesten = $artiesten;
+    $this->release_datum = $release_datum;
+    $this->url = $url;
+    $this->afbeelding = $afbeelding;
+    $this->prijs  = $prijs;
+
+}
+
+public static function  getAll(PDO $db): array
+{
+    $stnt= $db->query("SELECT * FROM album");
+
+      $albums = [];
+
+      while ($row = $stnt->fetch(PDO::FETCH_ASSOC))
+      {
+         $album = new Album(
+             $row['id'],
+             $row['naam'],
+             $row['artisten'],
+             $row['relaease_datum'],
+             $row['url'],
+             $row['afbeelding'],
+             $row['prijs']
+         );
+         $albums[] = $album;
+
+      }
+    return $albums;
+}
+public function save(PDO $db): void
+{
+        $stmt= $db->prepare("INSERT INTO album(naam, artiesten, release_datum, url ,afbeelding, prijs) VALUES (:naam, :artiesten, :release_datum, :url , :afbeelding, :prijs)" );
+        $stmt->bindParam(':id', $this->id);
+        $stmt->bindParam(':naam', $this->naam);
+        $stmt->bindParam(':artiesten', $this->artiesten);
+        $stmt->bindParam(':release_datum', $this->release_datum);
+        $stmt->bindParam(':url', $this->url);
+        $stmt->bindParam(':afbeelding', $this->afbeelding);
+        $stmt->bindParam(':prijs', $this->prijs);
+        $stmt->execute();
+}
+
+    public function update(PDO $db): void
+    {
+        $stmt= $db->prepare("UPDATE album SET naam = :naam, artiesten = :artiesten, release_datum = :release_datum, url = :url , afbeelding = :afbeelding, prijs = :prijs, WHERE id = :id");
+        $stmt->bindParam(':id', $this->id);
+        $stmt->bindParam(':naam', $this->naam);
+        $stmt->bindParam(':artiesten', $this->artiesten);
+        $stmt->bindParam(':release_datum', $this->release_datum);
+        $stmt->bindParam(':url', $this->url);
+        $stmt->bindParam(':afbeelding', $this->afbeelding);
+        $stmt->bindParam(':prijs', $this->prijs);
+        $stmt->execute();
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+    public function getNaam(): string
+    {
+        return $this->naam;
+    }
+
+    public function getArtiesten(): string
+    {
+        return $this->artiesten;
+    }
+
+    public function getRelease_datum(): ?string
+    {
+        return $this->release_datum;
+    }
+
+    public function getUrl(): ?string
+    {
+        return $this->url;
+    }
+
+    public function getAfbeelding(): ?string
+    {
+        return $this->afbeelding;
+    }
+
+    public function getPrijs(): ?string
+    {
+        return $this->prijs;
+    }
+
+    public function setNaam(string $naam): void
+    {
+        $this->naam = $naam;
+    }
+
+    public function setArtiesten(string $artiesten): void
+    {
+        $this->artiesten = $artiesten;
+    }
+
+}
+
